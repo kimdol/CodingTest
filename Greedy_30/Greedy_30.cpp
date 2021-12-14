8cbff7b009b50850aa133383551bed8345f92174
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-12-15 00:11:48 +0900

    combination 진행중

--- Greedy_30/Greedy_30.cpp
+++ Greedy_30/Greedy_30.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <list>
 #include <algorithm>
+#include <cmath>
 
 
 class CombiningFindSystem
@@ -42,16 +43,30 @@ private:
         int max = *(input.begin());
         input.pop_front();
         std::cout << max;
-
+        //제일 큰 수를 제외한 n - 1개의 숫자들을 큰 순서대로 메모리에 저장
         Combination(n - 1, input, output);
-
-        std::list<int> tmp = output;
-        for (int i = 0; i < n + 1; i++)
+        
+        std::list<int> saveList = output;
+        for (int i = n; i > 0; i--)
         {
-            
+            std::list<int> tmp = saveList;
+            // 제일 큰 수를 사이사이에 붙이기
+            for (auto& elem : tmp)
+            {
+                /*int k{ i };
+                while (k != n)
+                {
+                    int digits = elem / std::pow((double)10, (double)k-1);
+                    elem += (std::pow((double)10, (double)k-1) * digits);
+                    k++;
+                }
+                elem += (std::pow((double)10, (double)n-1) * max);*/
 
+            }
+            // 합체
+            // output.merge(tmp);
+            output.insert(output.end(), tmp.begin(), tmp.end());
         }
-
     }
 
 };
