eb28ba21274c28af037ce430a365139f12be8117
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2022-01-10 21:39:03 +0900

    홀수개일때 완성

---
+++ Greedy_RollerCoaster/Greedy_RollerCoaster.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    int r{}, c{};
+    std::cin >> r >> c;
+
+    std::vector<int> arr(r * c);
+    for (int i = 0; i < r; i++)
+    {
+        for (int k = 0; k < c; k++)
+        {
+            std::cin >> arr[i * c + k];
+        }
+    }
+
+    /*****************핵심********************/
+    std::vector<std::string> ans{};
+    for (int i = 0; i < r; i++)
+    {
+        int cnt{ 1 };
+        std::string mv{};
+        while (cnt++ < c)
+        {
+            (i % 2) ? (mv +="L") : (mv += "R");
+        }
+        (i == r - 1) ? (mv) : (mv += "D");
+
+
+        ans.push_back(mv);
+    }
+
+    /******************************************/
+    for (auto elem : ans)
+    {
+        std::cout << elem;
+    }
+    std::cout << std::endl;
+
+}
