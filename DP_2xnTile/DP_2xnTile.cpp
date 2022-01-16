e1a18161ecf89d8e12393395aad65e4618f1c393
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2022-01-16 22:41:02 +0900

    완성

---
+++ DP_2xnTile/DP_2xnTile.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    // d[n] = d[n-1] + d[n-2]
+    int n;
+    std::cin >> n;
+
+    std::vector<int> d(n + 1);
+    d[1] = 1;
+    d[2] = 2;
+    for (int i = 3; i <= n; i++)
+    {
+        d[i] = (d[i - 1] + d[i - 2]) % 10007;
+    }
+
+    std::cout << d[n];
+}
