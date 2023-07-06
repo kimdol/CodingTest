17cb3f49609cc1395af87d2a5ebd8d77cbb85b87
kimdol <86827391+kimdol@users.noreply.github.com>
2023-07-07 01:18:05 +0900

    Fixed Solution to Topological Sorting

--- TopologicalSorting/Baekjoon/2252/2252.cpp
+++ TopologicalSorting/Baekjoon/2252/2252.cpp
@@ -1,6 +1,29 @@
 ﻿#include <iostream>
+#include <vector>
+#include <queue>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::queue<int> q;
+    std::vector<std::vector<int>> v(10, std::vector<int>(10));
+    int check[10]{};
+
+    while (!q.empty())
+    {
+        int start = q.front();
+        q.pop();
+
+        std::cout << start << " ";
+        
+        for (auto e : v[start])
+        {
+            check[e]--;
+
+            if (check[e] == 0)
+            {
+                q.push(e);
+            }
+        }
+    }
+
 }
