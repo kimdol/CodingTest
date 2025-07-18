b40f864b343411bbb341f145d61afa8b15a8ecdb
kimdol <86827391+kimdol@users.noreply.github.com>
2025-07-18 23:25:30 +0900

    start coding note

---
+++ CodingTestNote/note1.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+// 백트래킹 함수
+void MaxCnt1(const vector<vector<int>>& quest, int currentXp, int qCnt, vector<bool>& visits, int& maxValue)
+{
+    
+}
+
+int solution1(vector<vector<int>> quest)
+{
+    return 0;
+}
---
+++ CodingTestNote/note2.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+namespace WallAndPoison
+{
+    // 백트래킹 함수
+    void MaxCnt(const vector<vector<int>>& quest, int currentXp, int qCnt, vector<bool>& visits, int& maxValue)
+    {
+        
+    }
+
+    int solution(vector<vector<int>> quest)
+    {
+        int maxValue = 0;
+
+        return maxValue;
+    }
+}
